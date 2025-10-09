70fab8fb1735cf4707aaad0f5914b4e1dd44882a
hirai <whiskey.gokugoku@gmail.com>
2025-10-09 19:43:31 +0900

    10/9 correction

--- ECsite_project/product_list/views.py
+++ ECsite_project/product_list/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class IndexView(TemplateView):
+    template_name = "product_list/product_list.html"
+
+
+product_list = IndexView.as_view()
