28c90869bd3e2b3fcd6440087632ba63ae9553f8
hirai <whiskey.gokugoku@gmail.com>
2025-10-13 14:38:04 +0900

    10/13 productlist completed

--- config/urls.py
+++ config/urls.py
@@ -18,6 +18,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
+from . import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -28,3 +30,6 @@ urlpatterns = [
         name="product_list",
     ),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
