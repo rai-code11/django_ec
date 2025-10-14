27e4ae3f39bcc24d75d51100882154de393ec9b3
hirai <whiskey.gokugoku@gmail.com>
2025-10-15 08:47:41 +0900

    10/15 git push heroku:main

---
+++ config/settings/local.py
@@ -0,0 +1,29 @@
+from .base import *
+
+
+environ.Env.read_env(env_file=str(BASE_DIR) + "/.env")
+
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env("SECRET_KEY")
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+
+ALLOWED_HOSTS = ["*"]  # "*"に設定する
+
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "db",
+        "PORT": 5432,
+        "TIME_ZONE": "Asia/Tokyo",
+    }
+}
