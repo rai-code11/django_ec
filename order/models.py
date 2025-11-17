6bb77b522f4ad90b9a2d9df55802a8168475238a
hirai <whiskey.gokugoku@gmail.com>
2025-11-17 11:31:28 +0900

    create LineItem Model

--- order/models.py
+++ order/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from checkout.models import CartItem
 
 # Create your models here.
 
@@ -31,3 +32,16 @@ class Payment(models.Model):
     card_number = models.CharField("カード番号", max_length=19)
     expiration_date = models.CharField("有効期限", max_length=10)
     cvv = models.CharField("セキュリティコード", max_length=3)
+
+
+# 注文商品の明細モデル
+class LineItem(models.Model):
+    class Meta:
+        db_table = "line_item"
+
+    checkout = models.ForeignKey(
+        Checkout, verbose_name="注文情報", on_delete=models.CASCADE
+    )
+    cart_item_name = models.CharField("商品名", max_length=100)
+    cart_item_price = models.IntegerField("価格", default=0)
+    cart_item_quantity = models.IntegerField("数量", default=1)
