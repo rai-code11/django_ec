e4705ad916c83f6ec43734c686de8da158ff964d
hirai <whiskey.gokugoku@gmail.com>
2026-02-18 18:04:06 +0900

    Fix expired SendGrid API key issue

--- config/settings/production.py
+++ config/settings/production.py
@@ -14,6 +14,10 @@ EMAIL_HOST = "smtp.sendgrid.net"
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "apikey"
 # ここにapi-keyを入れる
-EMAIL_HOST_PASSWORD = env("SENDGRID_API_KEY")
+EMAIL_HOST_PASSWORD = env("SENDGRID_API_KEY", default="")
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = "hirai.fpchr064@gmail.com"
+
+
+if not EMAIL_HOST_PASSWORD:
+    EMAIL_BACKEND = "django.core.mail.backends.dummy.EmailBackend"
